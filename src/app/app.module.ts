5321e87624617c3592eaba3d98afb7c0673cd967
Norberto Caceres <norbe1994@yahoo.com>
2019-04-12 16:08:04 -0500

    generar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PastComponent } from './training/past/past.component';
+import { CurrentComponent } from './training/current/current.component';
+import { NewComponent } from './training/new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    WelcomeComponent,
+    PastComponent,
+    CurrentComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
