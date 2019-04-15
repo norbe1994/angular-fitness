5e1792dcaf5cec92c40584c8c02f68810124a826
Norberto Caceres <norbe1994@yahoo.com>
2019-04-15 14:31:16 -0500

    agregar funcionalidad de confirmar o cancelar cancelacion de ejercicio

--- src/app/training/current/current.component.ts
+++ src/app/training/current/current.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, EventEmitter, Output } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
 import { StopTrainingComponent } from './stop-training.component'
 
@@ -8,12 +8,17 @@ import { StopTrainingComponent } from './stop-training.component'
   styleUrls: ['./current.component.css'],
 })
 export class CurrentComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<any>()
   public progress: number = 0
   private timer: any
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.startTimer()
+  }
+
+  startTimer() {
     this.timer = setInterval(() => {
       this.progress += 5
       if (this.progress >= 100) {
@@ -24,6 +29,18 @@ export class CurrentComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer)
-    this.dialog.open(StopTrainingComponent)
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress,
+      },
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit()
+      } else {
+        this.startTimer()
+      }
+    })
   }
 }
