c37dace7f840525f2c3e69211e87f75806c392bb
Norberto Caceres <norbe1994@yahoo.com>
2019-04-15 16:13:57 -0500

    use service data for select options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HeaderComponent } from './navigation/header/header.component'
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component'
 import { StopTrainingComponent } from './training/current/stop-training.component'
 import { AuthService } from './providers/auth.service'
+import { TrainingService } from './providers/training.service'
 
 @NgModule({
   declarations: [
@@ -41,7 +42,7 @@ import { AuthService } from './providers/auth.service'
     FlexLayoutModule,
     FormsModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, TrainingService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent],
 })
--- src/app/training/new/new.component.ts
+++ src/app/training/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core'
+import { TrainingService } from 'src/app/providers/training.service'
+import { Exercise } from '../exercise.model'
 
 @Component({
   selector: 'app-new',
@@ -7,10 +9,13 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core'
 })
 export class NewComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>()
+  public availableExercises: Exercise[] = []
 
-  constructor() {}
+  constructor(private trainingService: TrainingService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.availableExercises = this.trainingService.availableExercises
+  }
 
   onStartTraining() {
     this.trainingStart.emit()
