12531727acd6f666e0e4b1b5eec7a2b03e376e3e
Norberto Caceres <norbe1994@yahoo.com>
2019-04-16 13:22:54 -0500

    escribir exercise a firestore

--- src/app/providers/training.service.ts
+++ src/app/providers/training.service.ts
@@ -44,13 +44,13 @@ export class TrainingService {
   }
 
   completeExercise() {
-    this.exercises.push({ ...this.runningExercise, date: new Date(), state: 'completed' })
+    this.addDataToDatabase({ ...this.runningExercise, date: new Date(), state: 'completed' })
     this.runningExercise = null
     this.exerciseChanged.next(null)
   }
 
   cancelExercise(progress: number) {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       duration: (this.runningExercise.duration * progress) / 100,
       calories: (this.runningExercise.calories * progress) / 100,
@@ -67,5 +67,15 @@ export class TrainingService {
 
   getCompletedOrCancelledExercises() {
     return this.exercises.slice()
+  }
+
+  private addDataToDatabase(data: Exercise) {
+    this.db
+      .collection('finishedExercises')
+      .add(data)
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => console.log(err))
   }
 }
