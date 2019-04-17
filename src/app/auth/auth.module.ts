6657216344d2277fd10624bd7687cfbd174ef5ab
Norberto Caceres <norbe1994@yahoo.com>
2019-04-17 11:20:02 -0500

    moduler auth routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { SignupComponent } from '../auth/signup/signup.component'
 
 import { AngularFireAuthModule } from 'angularfire2/auth'
 import { SharedModule } from '../shared/shared.module'
+import { AuthRoutingModule } from './auth-routing.module'
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [AngularFireAuthModule, SharedModule],
+  imports: [AngularFireAuthModule, SharedModule, AuthRoutingModule],
   exports: [],
 })
 export class AuthModule {}
