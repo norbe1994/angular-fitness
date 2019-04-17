482e8d56b235e438cc295e4c487e6c7a77f35ea8
Norberto Caceres <norbe1994@yahoo.com>
2019-04-17 11:42:04 -0500

    set firebase api key for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBYnEmOrPLmxYk69D1YFELQFvmu47EaAKs',
+    authDomain: 'angular-fitness-tracker-6e93f.firebaseapp.com',
+    databaseURL: 'https://angular-fitness-tracker-6e93f.firebaseio.com',
+    projectId: 'angular-fitness-tracker-6e93f',
+    storageBucket: 'angular-fitness-tracker-6e93f.appspot.com',
+    messagingSenderId: '943242215510',
+  },
+}
